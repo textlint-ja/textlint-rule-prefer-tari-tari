e11e0d298d8a6e474895314c88f5c00f84f5fb49
azu <azuciao@gmail.com>
2017-11-04 20:47:46 +0900

    docs(README): Update README

--- src/textlint-rule-prefer-tari-tari.js
+++ src/textlint-rule-prefer-tari-tari.js
@@ -78,7 +78,7 @@ const report = context => {
                         // console.log(afterSuru.nodeList);
                         report(
                             node,
-                            new RuleError(`EERR`, {
+                            new RuleError(`例示・並列・対表現において、片方の動詞が「〜たり」表現な場合は、もう片方の動詞も「〜たり」とします。`, {
                                 index: source.originalIndexFromIndex(afterSuru.position.index)
                             })
                         );
